9ebf5e5de26b34185dcf2b98f8aa8d64cfa6718f
Nidhi Kulkarni <nkulkarni@okta.com>
2013-08-13 23:05:33 +0000

    Add systemB

---
+++ systemB/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SystemB::Application.config.secret_token = 'f2bb82aaa1253d7276627c8a918f47159e6643018195b46c50407718e946734e8ad7fee52a6fcdaada27a50573214be60c7aa33a56086fd463ee8a050654f10d'
